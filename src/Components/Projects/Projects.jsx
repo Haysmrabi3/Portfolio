a97733df5d83fb8a5c4e50e7834f4962a77f3bfc
haysm <haysmrabi325@gmail.com>
2025-01-05 16:42:21 +0200

    adding a new project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Ecommerce from '../../Assets/Projects/E-commerce/Home.png'
 import Yummy from '../../Assets/Projects/Yummy/Home.png'
+import Fokir from '../../Assets/Projects/Fokir/fokir.png'
 import Bootstrap from "../../Assets/Skills/icons8-bootstrap.svg";
 import HTML from "../../Assets/Skills/html5.a7362949.svg";
 import  CSS  from '../../Assets/Skills/icons8-css.svg';
@@ -67,6 +68,26 @@ export default function Projects() {
               <li><img src={HTML} alt="skill image" /></li>
               <li><img src={CSS} alt="skill image" /></li>
               <li><img src={Bootstrap} alt="skill image" /></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="Project mb-2"  data-aos="fade-up"  data-aos-delay='800'>
+            <div className="projec-img ">
+              <img src={Fokir} className='w-100'  alt="Poriject Imge" />
+            </div>
+            <div className="poject-text">
+            <h3>Fokir</h3>
+            <div className="icons">
+            <a target='_blank' href={`https://haysmrabi3.github.io/Fokir/`}><i className="fa-regular fa-eye"></i></a>
+            <a target='_blank' href={`https://github.com/Haysmrabi3/Fokir`}><i className="fa-brands fa-github"></i></a>
+            </div>
+            </div>
+            <hr />
+            <ul className='skills'>
+              <li><img src={HTML} alt="skill image" /></li>
+              <li><img src={CSS} alt="skill image" /></li>
             </ul>
           </div>
         </div>
