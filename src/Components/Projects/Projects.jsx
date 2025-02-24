3def8c91579efee3ae13064c0c24e073528add7e
Haysm <haysmrani325@gamil.com>
2025-02-24 13:49:40 +0200

    addin a new project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react'
 import Ecommerce from '../../Assets/Projects/E-commerce/Home.png'
 import Yummy from '../../Assets/Projects/Yummy/Home.png'
 import Fokir from '../../Assets/Projects/Fokir/fokir.png'
+import Mealfy from '../../Assets/Projects/Mealfy/Mealfy.png'
+import Weather from '../../Assets/Projects/Weather/Weather.png'
 import Englsih from '../../Assets/Projects/English-track/Screenshot 2025-02-10 103042.png'
 import Bootstrap from "../../Assets/Skills/icons8-bootstrap.svg";
 import HTML from "../../Assets/Skills/html5.a7362949.svg";
 import  CSS  from '../../Assets/Skills/icons8-css.svg';
+import  Js  from '../../Assets/Skills/javascript.455da02c.svg';
 import ReactLogo from "../../Assets/Skills/react.svg";
 import Aos from 'aos';
 
@@ -111,6 +114,47 @@ export default function Projects() {
             <ul className='skills'>
               <li><img src={HTML} alt="skill image" /></li>
               <li><img src={CSS} alt="skill image" /></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="Project mb-2"  data-aos="fade-up"  data-aos-delay='800'>
+            <div className="projec-img ">
+              <img src={Mealfy} className='w-100'  alt="Poriject Imge" />
+            </div>
+            <div className="poject-text">
+            <h3>Mealfy</h3>
+            <div className="icons">
+            <a target='_blank' href={`https://haysmrabi3.github.io/Mealfy/`}><i className="fa-regular fa-eye"></i></a>
+            <a target='_blank' href={`https://github.com/Haysmrabi3/Mealfy`}><i className="fa-brands fa-github"></i></a>
+            </div>
+            </div>
+            <hr /> 
+            <ul className='skills'>
+              <li><img src={HTML} alt="skill image" /></li>
+              <li><img src={CSS} alt="skill image" /></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="Project mb-2"  data-aos="fade-up"  data-aos-delay='800'>
+            <div className="projec-img ">
+              <img src={Weather} className='w-100'  alt="Poriject Imge" />
+            </div>
+            <div className="poject-text">
+            <h3>Weather App</h3>
+            <div className="icons">
+            <a target='_blank' href={`https://haysmrabi3.github.io/Weather-App/`}><i className="fa-regular fa-eye"></i></a>
+            <a target='_blank' href={`https://github.com/Haysmrabi3/Weather-App`}><i className="fa-brands fa-github"></i></a>
+            </div>
+            </div>
+            <hr /> 
+            <ul className='skills'>
+              <li><img src={HTML} alt="skill image" /></li>
+              <li><img src={CSS} alt="skill image" /></li>
+              <li><img src={Js} alt="skill image" /></li>
             </ul>
           </div>
         </div>
